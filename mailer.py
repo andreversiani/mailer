62b72e176392783267e13b6986231d6318f1987f
lumini23 <lumini2@hotmail.com.br>
2022-02-02 18:34:48 -0300

    feat: add a method to create a summary sheet

--- mailer.py
+++ mailer.py
@@ -91,6 +91,22 @@ class Emailer:
                     email.Attachments.Add(attachment)
         email.CC = 'comercialenergia@visionsistemas.com.br'
 
+    def create_summary_sheet(self, fornecedor, wb):
+        sheet = wb['Cobrar Cotações']
+
+        for row in range(3, 100):
+
+            a_cell = sheet[f'A{row}']
+            b_cell = sheet[f'B{row}']
+            c_cell = sheet[f'C{row}']
+
+            if a_cell.value == None:
+                a_cell.value = fornecedor
+                print(row, a_cell.value)
+                b_cell.value = 'Não'
+                c_cell.value = self.subject
+                break
+
 
 def get_et():
     window = Tk()
@@ -113,12 +129,10 @@ def find_voltage_class(num):
         return 'UAT'
 
 
-def get_data_from_equipamentos_sheet():
+def get_data_from_equipamentos_sheet(wb):
     window = Tk()
     window.eval('tk::PlaceWindow . center')
     window.withdraw()
-    filename = askopenfilename()
-    wb = load_workbook(filename, data_only=True)
     window.destroy()
     equipamentos = []
 
@@ -183,106 +197,118 @@ class Main_widget():
         self.root.destroy()
 
 
-def init():
+def make_fornecedores_resumo(wb):
+    equipamentos, FORNECEDORES = get_data_from_equipamentos_sheet(wb)
+    resumo = {}
+    for fornecedor in FORNECEDORES:
+        for equipamento in equipamentos:
+            try:
+                validator = FORNECEDORES[fornecedor][equipamento.tensao][equipamento.type]
+            except Exception as e:
+                print(e)
+            if validator:
+                if fornecedor not in resumo:
+                    resumo.update(
+                        {fornecedor: []})
+                resumo[fornecedor].append(equipamento)
+
+    return resumo, FORNECEDORES
+
+
+def build():
     main_widget = Main_widget()
-    equipamentos, FORNECEDORES = get_data_from_equipamentos_sheet()
-    return main_widget, equipamentos, FORNECEDORES
+    Tk().withdraw()
+    wb_name = askopenfilename()
+    wb = load_workbook(wb_name, data_only=True, keep_vba=True)
 
+    resumo, FORNECEDORES = make_fornecedores_resumo(wb)
 
-main_widget, equipamentos, FORNECEDORES = init()
-resumo = {}
-for fornecedor in FORNECEDORES:
-    for equipamento in equipamentos:
-        try:
-            validator = FORNECEDORES[fornecedor][equipamento.tensao][equipamento.type]
-        except Exception as e:
-            print(e)
-        if validator:
-            if fornecedor not in resumo:
-                resumo.update(
-                    {fornecedor: []})
-            resumo[fornecedor].append(equipamento)
+    for fornecedor in resumo:
+        contatos = FORNECEDORES[fornecedor]['contatos']
+        hora = datetime.datetime.now().ctime()
+        hora = int(hora[11:13])
 
-for fornecedor in resumo:
+        if hora in range(0, 13):
+            introducao = 'bom dia'
+        if hora in range(13, 18):
+            introducao = 'boa tarde'
+        if hora in range(18, 24):
+            introducao = 'boa noite'
 
-    contatos = FORNECEDORES[fornecedor]['contatos']
-    hora = datetime.datetime.now().ctime()
-    hora = int(hora[11:13])
+        if len(contatos) > 1:
+            tratamento = 'Prezados'
+        if len(contatos) == 1:
+            tratamento = contatos[0]['tratamento'].capitalize() + \
+                ' ' + contatos[0]['nome']
 
-    if hora in range(0, 13):
-        introducao = 'bom dia'
-    if hora in range(13, 18):
-        introducao = 'boa tarde'
-    if hora in range(18, 24):
-        introducao = 'boa noite'
+        date = str(datetime.date.today() +
+                   datetime.timedelta(days=int(main_widget.dias)))
+        year = date[0:4]
+        month = date[5:7]
+        date = date[9:]
+        date = f'{date}/{month}/{year}'
 
-    if len(contatos) > 1:
-        tratamento = 'Prezados'
-    if len(contatos) == 1:
-        tratamento = contatos[0]['tratamento'].capitalize() + \
-            contatos[0]['nome']
+        text = ''
+        item = 1
+        ets = []
+        for eq in resumo[fornecedor]:
 
-    date = str(datetime.date.today() +
-               datetime.timedelta(days=int(main_widget.dias)))
-    year = date[0:4]
-    month = date[5:7]
-    date = date[9:]
-    date = f'{date}/{month}/{year}'
+            equipamentos = []
 
-    text = ''
-    item = 1
-    ets = []
-    for eq in resumo[fornecedor]:
+            if eq.type not in equipamentos:
+                equipamentos.append(eq.type.capitalize())
 
-        equipamentos = []
+            if eq.et not in ets:
+                ets.append(eq.et)
 
-        if eq.type not in equipamentos:
-            equipamentos.append(eq.type.capitalize())
+            TEXT_COLOR = '#3c4064'
+            TABLE_STYLE = 'border-collapse:collapse; margin: 5px; font-size: 14px; width: 500px;'
+            TR_STYLE = 'background-color:#154c79; color: white; font-weight:bold;border: 1px solid; border: 1px solid'
+            TH_STYLE = 'padding: 1px 4px; border: 1px solid'
+            TD_STYLE = 'text-align:center; padding: 1px 4px; border: 1px solid; background-color: white'
 
-        if eq.et not in ets:
-            ets.append(eq.et)
-
-        TEXT_COLOR = '#3c4064'
-        TABLE_STYLE = 'border-collapse:collapse; margin: 5px; font-size: 14px; width: 500px;'
-        TR_STYLE = 'background-color:#154c79; color: white; font-weight:bold;border: 1px solid; border: 1px solid'
-        TH_STYLE = 'padding: 1px 4px; border: 1px solid'
-        TD_STYLE = 'text-align:center; padding: 1px 4px; border: 1px solid; background-color: white'
-
-        text += f"""
-        <tr'>
-            <td style='{TD_STYLE}'>{item}</td>
-            <td style='{TD_STYLE}'>{eq.descricao}</td>
-            <td style='{TD_STYLE}'>{eq.qtd}</td>
-        </tr>
-        """
-        item += 1
-        body = f"""
-                <div>
-                <p style='color: {TEXT_COLOR}'p>{tratamento}, {introducao}, </p>
-                <p style='color: {TEXT_COLOR}'p>Devido à necessidade do setor comercial da Vision Engenharia, solicitamos o orçamento dos equipamentos destacados na tabela a seguir. O anexo contém mais informações a serem consideradas.</p>
-                <table style='{TABLE_STYLE}'>
-                        <tr style='{TR_STYLE}'>
-                            <th style='{TH_STYLE}'>ITEM</th>
-                            <th style='{TH_STYLE}'>DESCRIÇÃO</th>
-                            <th style='{TH_STYLE}'>QTD</th>
-                        </tr>
-                    {text}   
-                </table>
-                    <p style='color: {TEXT_COLOR}'>O prazo máximo para a cotação é dia {date}.</p>
-                    <p style='color: {TEXT_COLOR}'>Observações:</p>
-                    <p style='color: {TEXT_COLOR}'>- Caso não seja possível realizar o orçamento dentro do prazo, favor informar em resposta a este e-mail;</p>
-                    <p style='color: {TEXT_COLOR}'>- Favor responder a todos os que estão em cópia.</p>
-                    <p style='color: {TEXT_COLOR}'>Desde já, a Vision agradece o seu apoio e a sua atenção e nos colocamos à disposição para sanar quaisquer dúvidas sobre o orçamento.</p>
-                </div>
+            text += f"""
+            <tr'>
+                <td style='{TD_STYLE}'>{item}</td>
+                <td style='{TD_STYLE}'>{eq.descricao}</td>
+                <td style='{TD_STYLE}'>{eq.qtd}</td>
+            </tr>
             """
+            item += 1
+            body = f"""
+                    <div>
+                    <p style='color: {TEXT_COLOR}'p>{tratamento}, {introducao}, </p>
+                    <p style='color: {TEXT_COLOR}'p>Devido à necessidade do setor comercial da Vision Engenharia, solicitamos o orçamento dos equipamentos destacados na tabela a seguir. O anexo contém mais informações a serem consideradas.</p>
+                    <table style='{TABLE_STYLE}'>
+                            <tr style='{TR_STYLE}'>
+                                <th style='{TH_STYLE}'>ITEM</th>
+                                <th style='{TH_STYLE}'>DESCRIÇÃO</th>
+                                <th style='{TH_STYLE}'>QTD</th>
+                            </tr>
+                        {text}   
+                    </table>
+                        <p style='color: {TEXT_COLOR}'>O prazo máximo para a cotação é dia {date}.</p>
+                        <p style='color: {TEXT_COLOR}'>Observações:</p>
+                        <p style='color: {TEXT_COLOR}'>- Caso não seja possível realizar o orçamento dentro do prazo, favor informar em resposta a este e-mail;</p>
+                        <p style='color: {TEXT_COLOR}'>- Favor responder a todos os que estão em cópia.</p>
+                        <p style='color: {TEXT_COLOR}'>Desde já, a Vision agradece o seu apoio e a sua atenção e nos colocamos à disposição para sanar quaisquer dúvidas sobre o orçamento.</p>
+                    </div>
+                """
 
-    to = []
-    for contato in contatos:
-        to.append(contato['email'])
-    to = ';'.join(to)
+        to = []
+        for contato in contatos:
+            to.append(contato['email'])
+        to = ';'.join(to)
 
-    equipamentos = ', '.join(equipamentos)
+        equipamentos = ', '.join(equipamentos)
 
-    subject = f'0006 | RFQ{main_widget.num_proposta} - Cotação {equipamentos} [{fornecedor}]'
-    emailer = Emailer(to, subject, body, ets)
-    emailer.prepare_emails()
+        subject = f'0006 | RFQ{main_widget.num_proposta} - Cotação {equipamentos} [{fornecedor}]'
+        emailer = Emailer(to, subject, body, ets)
+        emailer.prepare_emails()
+
+        emailer.create_summary_sheet(fornecedor, wb)
+
+        wb.save(wb_name)
+
+
+build()
